c116f89df070173eb05d6bbdbb785993546bd694
Admin <@schckuratova.mar@yandex.ru>
2024-11-18 15:54:05 +0500

    adding ratings to the database

--- src/pages/ThemeDetails/ThemeDetails.jsx
+++ src/pages/ThemeDetails/ThemeDetails.jsx
@@ -1,85 +1,134 @@
-import React, { useEffect, useState } from "react";
-import { Link, useLocation } from "react-router-dom";
+import React, { useState, useEffect } from "react";
 import axios from "axios";
+import { useLocation } from "react-router-dom";
 import styles from "./ThemeDetails.module.css";
 import { url } from "../../costants/constants";
 
 function ThemeDetails() {
-  const [themeDetails, setThemeDetails] = useState(null);
   const [students, setStudents] = useState([]);
+  const [themeDetails, setThemeDetails] = useState(null);
+  const [localEstimates, setLocalEstimates] = useState({});
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(null);
-  const [notes, setNotes] = useState(""); // Для хранения заметок преподавателя
   const location = useLocation();
 
   const { classId, className, subjectName, themeName, themeId } =
     Object.fromEntries(new URLSearchParams(location.search));
 
+  // Функция для получения CSS-класса по оценке
+  const getScoreClass = (score) => {
+    switch (score) {
+      case "5":
+        return styles.score5;
+      case "4":
+        return styles.score4;
+      case "3":
+        return styles.score3;
+      case "2":
+        return styles.score2;
+      case "н":
+        return styles.scoreН;
+      case "б":
+        return styles.scoreБ;
+      default:
+        return "";
+    }
+  };
+
+  // Загрузка студентов
   const fetchStudents = async () => {
     try {
       const response = await axios.get(
         `${url}/student/getStudentsByClassId/${classId}`
       );
       setStudents(response.data);
-    } catch (err) {
+    } catch {
       setError("Ошибка при загрузке данных студентов");
     }
   };
 
-  useEffect(() => {
-    const fetchThemeDetails = async () => {
-      try {
-        const response = await axios.get(`${url}/theme/${themeId}`);
-        setThemeDetails(response.data);
-      } catch (err) {
-        setError("Ошибка при загрузке данных темы");
-      } finally {
-        setLoading(false);
-      }
-    };
+  // Загрузка данных темы
+  const fetchThemeDetails = async () => {
+    try {
+      const response = await axios.get(`${url}/theme/${themeId}`);
+      setThemeDetails(response.data);
+    } catch {
+      setError("Ошибка при загрузке данных темы");
+    } finally {
+      setLoading(false);
+    }
+  };
 
+  // Загрузка оценок с сервера
+  const fetchThemeJournal = async () => {
+    try {
+      const response = await axios.get(`${url}/themeJournal/${themeId}`);
+
+      // Инициализация локальных оценок
+      const initialEstimates = response.data.reduce((acc, record) => {
+        acc[record.studentId] = {
+          estimation1: record.estimation1,
+          estimation2: record.estimation2,
+          estimation3: record.estimation3,
+          estimation4: record.estimation4,
+        };
+        return acc;
+      }, {});
+
+      setLocalEstimates(initialEstimates);
+    } catch {
+      setError("Ошибка при загрузке оценок");
+    }
+  };
+
+  // Обновление оценок
+  const handleScoreChange = (e, studentId, estimationKey) => {
+    const value = e.target.value;
+
+    setLocalEstimates((prev) => ({
+      ...prev,
+      [studentId]: {
+        ...prev[studentId],
+        [estimationKey]: value,
+      },
+    }));
+  };
+
+  // Сохранение оценок
+  const handleSave = async () => {
+    const recordsToSave = students.map((student) => {
+      const { estimation1, estimation2, estimation3, estimation4 } =
+        localEstimates[student.studentId] || {};
+      return {
+        studentId: student.studentId,
+        subjectId: themeDetails.subjectId,
+        themeId,
+        classId,
+        estimation1: estimation1 || null,
+        estimation2: estimation2 || null,
+        estimation3: estimation3 || null,
+        estimation4: estimation4 || null,
+      };
+    });
+
+    try {
+      await axios.post(
+        `${url}/themeJournal/addJournalRecord/${classId}-${themeId}`,
+        recordsToSave
+      );
+      alert("Оценки успешно сохранены");
+    } catch {
+      setError("Ошибка при сохранении оценок");
+    }
+  };
+
+  useEffect(() => {
     if (themeId) {
       fetchThemeDetails();
       fetchStudents();
+      fetchThemeJournal();
     }
-  }, [themeId, classId]);
-
-  const handleScoreChange = (e) => {
-    const selectCell = e.target.closest("td");
-    const selectElement = e.target;
-
-    const setColor = (color) => {
-      selectCell.style.backgroundColor = color;
-      selectElement.style.backgroundColor = color;
-    };
-
-    switch (e.target.value) {
-      case "5":
-        setColor("#D2FFBA");
-        break;
-      case "4":
-        setColor("#FBFF89");
-        break;
-      case "3":
-        setColor("#FFD38D");
-        break;
-      case "2":
-        setColor("#FFD2D2");
-        break;
-      case "н":
-        setColor("#FFBE86");
-        break;
-      case "б":
-        setColor("#FFA4A4");
-        break;
-      default:
-        setColor(""); // Сбрасываем цвет
-    }
-  };
-
-  const handleNoteChange = (e) => {
-    setNotes(e.target.value); // Обновляем заметки
-  };
+  }, [themeId]);
 
   if (loading) return <p>Загрузка...</p>;
   if (error) return <p>{error}</p>;
@@ -98,34 +147,58 @@ function ThemeDetails() {
             <thead>
               <tr>
                 <th>ФИО учащегося</th>
-                <th colSpan="5">Оценки</th>
+                <th colSpan="4" className={styles.center}>
+                  Оценки
+                </th>
               </tr>
             </thead>
             <tbody>
               {students.length > 0 ? (
-                students.map((student) => (
-                  <tr key={student.studentId}>
-                    <td>{student.fullName}</td>
-                    {Array(5)
-                      .fill("")
-                      .map((_, idx) => (
-                        <td key={idx} className={styles.selectCell}>
-                          <select
-                            className={styles.scoreSelect}
-                            onChange={(e) => handleScoreChange(e)}
-                          >
-                            <option value=""></option>
-                            <option value="5">5</option>
-                            <option value="4">4</option>
-                            <option value="3">3</option>
-                            <option value="2">2</option>
-                            <option value="н">Н</option>
-                            <option value="б">Б</option>
-                          </select>
-                        </td>
-                      ))}
-                  </tr>
-                ))
+                students.map((student) => {
+                  const record = localEstimates[student.studentId] || {};
+                  return (
+                    <tr key={student.studentId}>
+                      <td>{student.fullName}</td>
+                      {Array(4)
+                        .fill(" ")
+                        .map((_, idx) => {
+                          const estimationKey = `estimation${idx + 1}`;
+                          const value = record[estimationKey] || "";
+
+                          return (
+                            <td
+                              key={idx}
+                              className={`${styles.selectCell} ${getScoreClass(
+                                value
+                              )}`}
+                            >
+                              <select
+                                className={`${
+                                  styles.scoreSelect
+                                } ${getScoreClass(value)}`}
+                                value={value}
+                                onChange={(e) =>
+                                  handleScoreChange(
+                                    e,
+                                    student.studentId,
+                                    estimationKey
+                                  )
+                                }
+                              >
+                                <option value=""></option>
+                                <option value="5">5</option>
+                                <option value="4">4</option>
+                                <option value="3">3</option>
+                                <option value="2">2</option>
+                                <option value="н">Н</option>
+                                <option value="б">Б</option>
+                              </select>
+                            </td>
+                          );
+                        })}
+                    </tr>
+                  );
+                })
               ) : (
                 <tr>
                   <td colSpan="5">Студенты не найдены</td>
@@ -135,25 +208,12 @@ function ThemeDetails() {
           </table>
         </div>
 
-        <div className={styles.notesContainer}>
-          <h4>Заметки преподавателя</h4>
-          <textarea
-            className={styles.notesTextarea}
-            value={notes}
-            onChange={handleNoteChange}
-            placeholder="Введите заметки..."
-          />
+        <div className={styles.saveContainer}>
+          <button className={styles.saveButton} onClick={handleSave}>
+            Сохранить все оценки
+          </button>
         </div>
       </div>
-
-      <Link
-        to={{
-          pathname: "/rating",
-          search: `?classId=${classId}&className=${className}&themeName=${themeName}`,
-        }}
-      >
-        <button className={styles.button}>Рейтинг класса</button>
-      </Link>
     </div>
   );
 }
